16e61199a4f3df05cd3f0be1475132878990222c
hellojinoo <wkdwlsdn9637@gmail.com>
2023-08-25 10:42:53 +0900

    V4 추가

--- src/main/java/com/example/aefa/AA.java
+++ src/main/java/com/example/aefa/AA.java
@@ -4,11 +4,14 @@ public class AA {
     public String v1(){
         return "v1";
     }
-
     public String v2(){
         return "v2";
     }
     public String v3(){
         return "v3";
+    }
+
+    public String v4(){
+        return "v4";
     }
 }
